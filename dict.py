a4bdc8a1db45c67f8a762127eff5ed39e87dcfa3
Cornelius5555 <corneliuskiplagat5@gmail.com>
2023-11-21 11:12:50 +0300

    update

--- dict.py
+++ dict.py
@@ -16,9 +16,27 @@ print(my_dict)
 #task
 my_ds = [23, "Jane", (560), ["Lesson", "Maths", {"currency" : "KES"}], 987, (76,"John")]
 
+#print kes
+print(my_ds[3][2]["currency"])
+
+#print 560
+print(my_ds[2])
+
+#print maths
+print(my_ds[3][1])
+#In the dictionary with the key currency, add another key “amount” with value 90
+my_ds[3][2]["amount"] = 90
+print(my_ds)
+
+#Reverse 987 to 789 without using an inbuilt -method or Assigning 789 manually.
+ #    Hint: Strings can be reversed using [::]
+my_ds[4] = str(my_ds[4])
+print(my_ds[4][2::-1])
+
+
 
 #Change the name “John” to “Jane” .
-my_ds[5] = list(my_ds)
+my_ds[5] = list(my_ds[5])
 my_ds[5][1] = "Jane"
 my_ds[5] = tuple(my_ds[5])
 print(my_ds)
